39f30b6552eeae2ebc6065aaa16c21d10a1a6382
Hari Krishnan <harikrishnan83@gmail.com>
2022-11-14 11:22:14 +0530

    test

--- backward_compatibility.sh
+++ backward_compatibility.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "$1" | tr -s ' ' '\n' | xargs -I % java -jar ./tools/specmatic.jar compatible git commits % main HEAD
+echo "$1" | tr -s ' ' '\n' | xargs -I % echo % main HEAD
+#echo "$1" | tr -s ' ' '\n' | xargs -I % java -jar ./tools/specmatic.jar compatible git commits % main HEAD
