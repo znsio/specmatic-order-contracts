a2851f460b13fcd13a3bdfcfadedbd58c053c9df
Hari Krishnan <harikrishnan83@gmail.com>
2022-11-14 10:23:04 +0530

    passing changed files list as argument

--- backward_compatibility.sh
+++ backward_compatibility.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -jar ./tools/specmatic.jar compatible git commits in/specmatic/examples/store/api_order_v1.yaml main HEAD
+cat "The files that changed: $1"
+#java -jar ./tools/specmatic.jar compatible git commits in/specmatic/examples/store/api_order_v1.yaml main HEAD
